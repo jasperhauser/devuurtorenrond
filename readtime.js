d5c3fce3a35c15f46e86c99eeaa38a4135df66b8
jasperhauser <jasper.rh@gmail.com>
2023-02-13 21:09:28 +0100

    Had realtime stuk gemaakt voor winter artikel

--- readtime.js
+++ readtime.js
@@ -1,8 +1,12 @@
 function readingTime() {
+    // Get the text from the article
     const text = document.getElementById("article").innerText;
+    // Set the reading speed
     const wpm = 225;
+    // Calculate the reading time
     const words = text.trim().split(/\s+/).length;
     const time = Math.ceil(words / wpm);
+    // Display the reading time
     document.getElementById("readingTime").innerText = time;
 }
 readingTime();
